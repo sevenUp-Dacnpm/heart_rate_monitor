01f6bdeae229531d68aee4d2edb8de48c22a097e
LinhDong64 <mailinhdong@gmail.com>
2021-05-05 00:07:46 +0700

    modify reminder tab layout and add a showDatePicker in it

--- lib/screens/main_screen/reminder_tab/reminder_tab.dart
+++ lib/screens/main_screen/reminder_tab/reminder_tab.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ReminderTab extends StatefulWidget {
   @override
@@ -6,6 +9,31 @@ class ReminderTab extends StatefulWidget {
 }
 
 class _ReminderTabState extends State<ReminderTab> {
+  var finaldate;
+  void callDatePicker() async {
+    var order = await getDate();
+    setState(() {
+      finaldate = order;
+    });
+  }
+
+  Future<DateTime?> getDate() {
+    // Imagine that this function is
+    // more complex and slow.
+    return showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2018),
+      lastDate: DateTime(2030),
+      // builder: (BuildContext context, Widget? child) {
+        // return Theme(
+          // data: ThemeData.light(),
+          // child: child
+        // );
+      // },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -20,35 +48,65 @@ class _ReminderTabState extends State<ReminderTab> {
           child: Column(
             children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'Date Time',
-                    style: Theme.of(context).textTheme.headline4,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Date Time',
+                          style: Theme.of(context).textTheme.headline4),
+                      Text(
+                        'Note',
+                        style: Theme.of(context).textTheme.headline4,
+                      )
+                    ],
                   ),
-                  Text('29/03/1999')
-                ],
-              ),
-              SizedBox(
-                height: screenSize.height * 0.05,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Note',
-                    style: Theme.of(context).textTheme.headline4,
+                  SizedBox(
+                    width: screenSize.height * 0.05,
                   ),
-                  Text('Hello world')
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text('$finaldate'),
+                          IconButton(
+                              icon: Icon(
+                                Icons.calendar_today_outlined,
+                                color: Color(0xFF84E0D4),
+                              ),
+                              onPressed: callDatePicker)
+                        ],
+                      ),
+                      Text('Hello world')
+                    ],
+                  )
                 ],
               ),
               SizedBox(
                 height: screenSize.height * 0.15,
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  TextButton(onPressed: null, child: Text('Cancel')),
+                  TextButton(
+                    onPressed: null,
+                    style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Color(0xFF000000)),
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Color(0xFFF5F5F5)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ))),
+                    child: Padding(
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        child: Text(
+                          'Cancel',
+                          style: Theme.of(context).textTheme.headline4,
+                        )),
+                  ),
                   TextButton(
                     style: ButtonStyle(
                         foregroundColor:
@@ -62,9 +120,11 @@ class _ReminderTabState extends State<ReminderTab> {
                         ))),
                     onPressed: () {},
                     child: Padding(
-                        padding: EdgeInsets.only(
-                            left: 20, right: 20, top: 5, bottom: 5),
-                        child: Text('Oke')),
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        child: Text(
+                          'Oke',
+                          style: Theme.of(context).textTheme.headline4,
+                        )),
                   )
                 ],
               )
